1405c3bdae73a26bd34b8fd5534a499eecec01b5
Tfsiong <tiongfs-wm23@student.tarc.edu.my>
2024-09-20 02:21:48 +0800

    update main

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        Staff.loginPage();
+        Person person = new Person();
+        Inventory inventory = new Inventory();
+
+        Person[] people = person.createPersonArray();
+        Branch[] branches = inventory.createBranchArray();
+        Staff.loginPage(people, branches);
         //ReadFromFile.main(args);
     }
 }
